237aad4a64647e1c936e3c2b98dba4fafb9557dc
Samuel Fish <fishsamuel0@gmail.com>
2025-11-28 22:23:26 +0000

    correct cropping

--- trim_video.sh
+++ trim_video.sh
@@ -15,22 +15,23 @@ END="$3"
 VIDNAME="${VID%.*}"
 OUTPUT="${VIDNAME}_trimmed.mov"
 
-CROP_ON_ORIGINAL="crop=156:112"
-LARGEST_16_9_CROP='crop=w=if(gt(iw/ih,16/9)\,trunc(ih*16/9/2)*2\,iw):h=if(gt(iw/ih,16/9)\,ih\,trunc(iw*9/16/2)*2):x=(iw-if(gt(iw/ih,16/9)\,trunc(ih*16/9/2)*2\,iw))/2:y=(ih-if(gt(iw/ih,16/9)\,ih\,trunc(iw*9/16/2)*2))/2'
+CROP_ON_ORIGINAL="crop=iw-156:ih-112:156:112"
 
-SCALE_FILTER="scale=1920:1080:flags=oversample" # upscaling filter
-FALLBACK="crop=1920:1080"
+LARGEST_16_9_CROP='crop=w=if(gt(iw/ih\,16/9)\,ih*16/9\,iw):h=if(gt(iw/ih\,16/9)\,ih\,iw*9/16):x=(iw-out_w)/2:y=(ih-out_h)/2'
+SCALE_FILTER='scale=1920:1080:flags=lanczos' # upscaling filter
+
+
 
 # for deinterlacing use -vf bwdif=1
 ffmpeg -i "$VID" -ss "$START" -to "$END" \
-  # CROPPING OPTION
-  #-vf "${CROP_ON_ORIGINAL},${LARGEST_16_9_CROP},${SCALE_FILTER},${FALLBACK}" \
+  -vf "${CROP_ON_ORIGINAL},${LARGEST_16_9_CROP},${SCALE_FILTER}" \
+  -c:v libx264 -preset veryfast -crf 18 -pix_fmt yuv420p -r 24 \
+  -c:a copy "$OUTPUT"
   # NEAR-LOSSLESS OPTION
   #-c:v prores_ks -profile:v 3 -pix_fmt yuv422p10le -r 24 \
-  # QUICK CHECK OPTION
-  -c:v libx264 -preset veryfast -crf 18 -pix_fmt yuv420p -r 24 \
-  # YOUTUBE ENCODING - change container to mp4 in OUTPUT variable
-  #-c:v libx264 -preset veryslow -crf 18 -pix_fmt yuv420p -r 24 -movflags +faststart \
-  -c:a copy "$OUTPUT"
   # STRAIGHT TO PNG FRAMES OPTION
   #-vsync 0 "${VIDNAME}_frames/%06d.png"
+  # YOUTUBE ENCODING - change container to mp4 in OUTPUT variable
+  #-c:v libx264 -preset veryslow -crf 18 -pix_fmt yuv420p -r 24 -movflags +faststart \
+  # FAST TESTING OPTION
+  #-c:v libx264 -preset veryfast -crf 18 -pix_fmt yuv420p -r 24 \
