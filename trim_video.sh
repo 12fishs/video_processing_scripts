1cf9305a5662b5cbb025b90ed47c7d26fb38f575
Samuel Fish <fishs@samuels-mini.home>
2025-11-28 14:01:40 +0000

    tested trimming script

--- trim_video.sh
+++ trim_video.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ $# -ne 3 ]; then
-    echo "Usage: $0 <video> <start> <end>. Time format: [HH:]MM:SS[.m...]."
+    echo "Trims video and reencodes with youtube in mind, copying the audio stream. Outputs to a *_trimmed.mp4."
+    echo "Usage: $0 <video> <start> <end>."
+    echo "Time format: [HH:]MM:SS[.m...]"
     exit 1
 fi
 
@@ -9,5 +11,9 @@ VID="$1"
 START="$2"
 END="$3"
 
+VIDNAME="${VID%.*}"
+OUTPUT="${VIDNAME}_trimmed.mp4"
+#
 # for deinterlacing use -vf bwdif=1
-ffmpeg -i "$VID" # -vf bwdif=1 deinterlacing apprently important??
+ffmpeg -i "$VID" -ss "$START" -to "$END" -c:v libx264 -preset veryslow -crf 18 -pix_fmt yuv420p \
+  -c:a copy "$OUTPUT"
